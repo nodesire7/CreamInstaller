e40a3671bd6c1f2083c7ab46af2c32f7b8aa9a4c
nodesire7 <63827263+nodesire7@users.noreply.github.com>
2026-08-08 17:08:03 +0800

    Simplify persisted language setting

--- CreamInstaller/Utility/Settings.cs
+++ CreamInstaller/Utility/Settings.cs
@@ -2,25 +2,9 @@ namespace CreamInstaller.Utility;
 
 internal sealed class SettingsModel
 {
-    private string language = LocalizationManager.Auto;
-
-    public SettingsModel()
-    {
-        LocalizationManager.Initialize(language);
-    }
-
     public bool UseSmokeAPI { get; set; } = true;
     public bool BlockProtectedGames { get; set; } = true;
     public bool DarkModeEnabled { get; set; } = true;
     public bool SortByName { get; set; } = true;
-
-    public string Language
-    {
-        get => language;
-        set
-        {
-            language = LocalizationManager.NormalizeConfiguredLanguage(value);
-            LocalizationManager.Initialize(language);
-        }
-    }
+    public string Language { get; set; } = LocalizationManager.Auto;
 }
