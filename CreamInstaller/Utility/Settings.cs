42b530f145202f12484d8980f5957a81c3e31d2d
nodesire7 <63827263+nodesire7@users.noreply.github.com>
2026-08-08 16:55:49 +0800

    initialize localization from settings

--- CreamInstaller/Utility/Settings.cs
+++ CreamInstaller/Utility/Settings.cs
@@ -2,9 +2,25 @@ namespace CreamInstaller.Utility;
 
 internal sealed class SettingsModel
 {
+    private string language = LocalizationManager.Auto;
+
+    public SettingsModel()
+    {
+        LocalizationManager.Initialize(language);
+    }
+
     public bool UseSmokeAPI { get; set; } = true;
     public bool BlockProtectedGames { get; set; } = true;
     public bool DarkModeEnabled { get; set; } = true;
     public bool SortByName { get; set; } = true;
-    public string Language { get; set; } = LocalizationManager.Auto;
+
+    public string Language
+    {
+        get => language;
+        set
+        {
+            language = LocalizationManager.NormalizeConfiguredLanguage(value);
+            LocalizationManager.Initialize(language);
+        }
+    }
 }
